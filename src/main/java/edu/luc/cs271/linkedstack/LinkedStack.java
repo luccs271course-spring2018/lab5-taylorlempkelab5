8292313361814636aa18495a50363511b65f2f91
Nick Taylor <31541603+ntaylor5@users.noreply.github.com>
2017-10-16 23:00:26 -0500

    Reupload

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -2,11 +2,7 @@ package edu.luc.cs271.linkedstack;
 
 import java.util.List;
 import java.util.ArrayList;
-<<<<<<< HEAD
-import java.util.*;
-=======
 import java.util.NoSuchElementException;
->>>>>>> 285bfe17c286f438361b85b4f708b43f85f5fa8e
 
 
 public class LinkedStack<E> implements IStack<E> {
@@ -20,26 +16,16 @@ public class LinkedStack<E> implements IStack<E> {
   public E push(final E obj)
   {
     // TODO
-<<<<<<< HEAD
-    // DONE
-    top = new Node<>(obj, top);
-    return null;
-=======
     top = new Node<>(obj, top);
     return obj;
->>>>>>> 285bfe17c286f438361b85b4f708b43f85f5fa8e
   }
 
   @Override
   public E peek()
   {
     // TODO
-<<<<<<< HEAD
-    if (isEmpty()){
-=======
     if (isEmpty())
     {
->>>>>>> 285bfe17c286f438361b85b4f708b43f85f5fa8e
       throw new NoSuchElementException();
     }
     return top.data;
@@ -49,18 +35,10 @@ public class LinkedStack<E> implements IStack<E> {
   public E pop()
   {
     // TODO
-<<<<<<< HEAD
-    // DONE
-    if(isEmpty()){
-      throw new NoSuchElementException();
-    }
-    else{
-=======
     if (isEmpty()) {
       throw new NoSuchElementException();
     }
     else {
->>>>>>> 285bfe17c286f438361b85b4f708b43f85f5fa8e
       E newTop = top.data;
       top = top.next;
       return newTop;
@@ -71,20 +49,10 @@ public class LinkedStack<E> implements IStack<E> {
   public boolean isEmpty()
   {
     // TODO
-<<<<<<< HEAD
-    // DONE
-    if (top == null){
-    return true;
-    }
-    else{
-    return false;
-    }
-=======
     if (top == null)
       {return true;}
     else
       {return false;}
->>>>>>> 285bfe17c286f438361b85b4f708b43f85f5fa8e
   }
 
   @Override
@@ -98,16 +66,6 @@ public class LinkedStack<E> implements IStack<E> {
       top = top.next;
     }
     // TODO add any instance variable(s) required to support this
-<<<<<<< HEAD
-    
-    
-    List<E> preAlList = new ArrayList<E>();
-    while(top != null){
-      preAlList.add(top.data);
-      top = top.next;
-    }
-=======
->>>>>>> 285bfe17c286f438361b85b4f708b43f85f5fa8e
     return preAlList;
   }
 }
--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -6,21 +6,12 @@ public class ReverseLines {
 
   public static void main(final String[] args) {
     // TODO read successive input lines until EOF, then print out in reverse order
-<<<<<<< HEAD
-    LinkedStack<String> stack = new LinkedStack<String>();
-
-    final Scanner input = new Scanner(System.in);
-    String line;
-    
-    while(input.hasNextLine())
-=======
 
     LinkedStack<String> stack = new LinkedStack<String>();
     final Scanner input = new Scanner(System.in);
     String line;
 
     while (input.hasNextLine())
->>>>>>> 285bfe17c286f438361b85b4f708b43f85f5fa8e
     {
       line = input.nextLine();
       stack.push(line);
@@ -28,14 +19,6 @@ public class ReverseLines {
       {
         break;
       }
-<<<<<<< HEAD
-    }
-    
-    while (!stack.isEmpty());
-    {
-      System.out.println(stack.pop());
-=======
->>>>>>> 285bfe17c286f438361b85b4f708b43f85f5fa8e
     }
 
     while (!stack.isEmpty())
@@ -44,4 +27,3 @@ public class ReverseLines {
     }
   }
 }
-
